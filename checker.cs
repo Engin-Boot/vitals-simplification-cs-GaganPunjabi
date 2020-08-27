8421302fc1c66618e3574256a59ec309f2dd1600
GaganPunjabi <45024902+GaganPunjabi@users.noreply.github.com>
2020-08-27 15:48:39 +0530

    Reduce cyclomatic complexity

--- checker.cs
+++ checker.cs
@@ -4,15 +4,31 @@ using System.Diagnostics;
 class Checker
 {
     static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
+        var flag1 = bpm_check(bpm);
+        var flag2 = spo_check(spo2);
+        var flag3 = resp_rate_check(respRate);
+        if (flag1 == flag2 ==flag3 == true)
+        {
+            return true;
         }
-        return true;
+        return false;
     }
+
+    static bool bpm_check(float bpm)
+    {
+        return !(bpm < 70 || bpm > 150);
+    }
+
+    static bool spo_check(float spo2)
+    {
+        return !(spo2 < 90);
+    }
+
+    static bool resp_rate_check(float respRate)
+    {
+        return !(respRate < 30 || respRate > 95);
+    }
+
     static void ExpectTrue(bool expression) {
         if(!expression) {
             Console.WriteLine("Expected true, but got false");
